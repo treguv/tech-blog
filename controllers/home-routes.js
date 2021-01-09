816caa693ce7158af6d6fd3063595572e09e36ef
treguv <vladislavtregubov00@gmail.com>
2021-01-09 02:14:40 -0800

    added template for dashboard page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,4 +5,14 @@ router.get("/", (req, res) => {
   res.render("home");
 });
 
+//serve up the login page
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+//serve up the dashboard
+router.get("/dashboard", (req, res) => {
+  res.render("dashboard");
+});
+
 module.exports = router;
