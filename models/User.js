71c45dea990cb2c8236cad26dc2f8719bbc422f5
treguv <vladislavtregubov00@gmail.com>
2021-01-09 02:59:46 -0800

    Buit models for comment, user, and post and set up their relations in index.js

---
+++ models/User.js
@@ -0,0 +1,50 @@
+// set up imports
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+//Set up object
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      // Validate if it is a valid email
+      validate: { isEmail: true },
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      //make sure password has to be at least 5 char
+      validate: { len: [5] },
+    },
+  },
+
+  {
+    //   Add bcrypt hooks here in the future to hash the password being put into db
+    // pass in our imported sequelize connection (the direct connection to our database)
+    sequelize,
+    // don't automatically create createdAt/updatedAt timestamp fields
+    timestamps: false,
+    // don't pluralize name of database table
+    freezeTableName: true,
+    // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
+    underscored: true,
+    // make it so our model name stays lowercase in the database
+    modelName: "user",
+  }
+);
+
+module.exports = User;
