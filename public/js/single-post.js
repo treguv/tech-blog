105891fa89777bef13b83d754f3034080e347274
treguv <vladislavtregubov00@gmail.com>
2021-01-10 03:24:13 -0800

    added ability to add comments onto the posts

---
+++ public/js/single-post.js
@@ -0,0 +1,34 @@
+async function submitPostHandler(event) {
+  event.preventDefault();
+  //get info we need
+  const post_id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
+  const comment_text = document.querySelector("#comment-text").value.trim();
+  const user_id = "1"; //TODO set to session auth
+  if (comment_text) {
+    //make sure we have comment text
+    const response = await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({
+        comment_text,
+        user_id,
+        post_id,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    //check if all good
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      alert(response.statusText); // find better way to do this
+    }
+  }
+}
+
+document
+  .querySelector("#post-comment-btn")
+  .addEventListener("click", submitPostHandler);
