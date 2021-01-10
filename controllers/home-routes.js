2e0dde625e0b82c5a6154c3f4ae5b5a8e3e8ca6f
treguv <vladislavtregubov00@gmail.com>
2021-01-10 02:04:00 -0800

    Added existing posts on home page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,38 @@
 const router = require("express").Router();
-
+const { User, Post, Comment } = require("../models");
+const sequelize = require("../config/connection");
 //home route server homepage
 router.get("/", (req, res) => {
-  res.render("home");
+  //we need to get all posts
+  Post.findAll({
+    attributes: ["id", "title", "body", "user_id"],
+    include: [
+      {
+        model: User,
+        as: "user",
+        attributes: ["username"],
+      },
+      {
+        model: Comment,
+        as: "comments",
+        attributes: ["id", "comment_text", "user_id"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      //serialize data
+      if (!dbPostData) {
+        res.status(404).json({ message: "No Posts Available" });
+        return;
+      }
+      const posts = dbPostData.map((post) => post.get({ plain: true })); // serialize all the posts
+      console.log(posts);
+      res.render("home", { posts });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 //serve up the login page
