fa95a1e6390b44404eae36fad618394ff0e84f5e
treguv <vladislavtregubov00@gmail.com>
2021-01-11 00:11:38 -0800

    added jaws db connection functionality

--- config/connection.js
+++ config/connection.js
@@ -3,16 +3,22 @@ const Sequelize = require("sequelize");
 
 require("dotenv").config(); // For loading env variables
 //create the connection to the database
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME, //vars set up in .env
-  process.env.DB_USER,
-  process.env.DB_PW,
-  {
-    host: "localhost",
-    dialect: "mysql",
-    port: 3306,
-  }
-);
+const sequelize;
+if (proccess.env.JAWSDB_URL) {
+  //we have jawsdb
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  //otherwise connect to local db
+  sequelize = new Sequelize(
+    process.env.DB_NAME, //vars set up in .env
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
 
 module.exports = sequelize;
